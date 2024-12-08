25b290bf68545931c76ba145888e491156e6a8e5
Arpit-Verma1 <arpitv747@gmail.com>
2024-12-08 18:48:33 +0530

    this is 2nd commit

--- SwiftGit/ContentView.swift
+++ SwiftGit/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
             List {
                 ForEach(items) { item in
                     NavigationLink {
-                       Text("this is 1st commit")
+                       Text("this is 2nd commit")
                     } label: {
 //                        Text(item.timestamp!, formatter: itemFormatter)
                     }
